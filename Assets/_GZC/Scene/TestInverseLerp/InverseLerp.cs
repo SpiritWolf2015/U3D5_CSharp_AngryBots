03abe8095cacabb77d958d53cbce5a0fcb7d4db6
SpiritWolf2015 <522414019@qq.com>
2015-05-18 00:58:39 +0800

    原主场景N多脚本丢失1.0
    原主场景N多脚本丢失，直接去除主相机，RainBox的MISS脚本。

---
+++ Assets/_GZC/Scene/TestInverseLerp/InverseLerp.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InverseLerp : MonoBehaviour {
+
+    public float walkSpeed = 5.0F;
+    public float runSpeed = 10.0F;
+    public float speed = 8.0F;
+
+    void Start() {
+        float parameter = Mathf.InverseLerp(walkSpeed, runSpeed, speed);
+        print(parameter);
+    }
+
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.A)) {
+            speed++;
+            float parameter = Mathf.InverseLerp(walkSpeed, runSpeed, speed);
+            print(parameter);
+        }
+        if (Input.GetKeyDown(KeyCode.S)) {
+            speed--;
+            float parameter = Mathf.InverseLerp(walkSpeed, runSpeed, speed);
+            print(parameter);
+        }
+    }
+
+}
---
+++ Assets/_GZC/Scene/TestRigidbody/TestRigidbody.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TestRigidbody : MonoBehaviour {
+
+    private Rigidbody m_Rigidbody;
+	
+	void Start () {
+        m_Rigidbody = GetComponent<Rigidbody>();
+	}	
+	
+	void OnGUI () {
+        if(Input.GetKey(KeyCode.Q)) {
+            m_Rigidbody.angularVelocity = Vector3.up;
+        }
+        if (Input.GetKey(KeyCode.W))  {
+            m_Rigidbody.angularVelocity = Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            m_Rigidbody.angularVelocity = -Vector3.forward;
+        }
+        if (Input.GetKey(KeyCode.D))  {
+            m_Rigidbody.angularVelocity = Vector3.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            m_Rigidbody.angularVelocity = -Vector3.right;
+        }
+	}
+
+}
